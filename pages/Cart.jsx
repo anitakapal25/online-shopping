09bee9ad10941f276dfa968bca5ba530228b2f92
anitakapal25 <49511269+anitakapal25@users.noreply.github.com>
2024-01-21 21:05:47 +0530

    Add files via upload

---
+++ pages/Cart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { CartItems } from '../components/cart_items/CartItems'
+
+export const Cart = () => {
+  return (
+    <div>
+      <CartItems/>
+    </div>
+  )
+}
